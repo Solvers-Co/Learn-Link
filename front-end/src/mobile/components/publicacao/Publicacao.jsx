47ce4d39d7d8e2163e8ad3fe38d5c4c4fe8f47bd
OtavioWalk <otavio.carvalho@sptech.school>
2024-08-18 18:25:57 -0300

    feat: Add edit and delete front-end to Publicacao component

--- front-end/src/mobile/components/publicacao/Publicacao.jsx
+++ front-end/src/mobile/components/publicacao/Publicacao.jsx
@@ -1,33 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Styles from '../publicacao/Publicacao.module.css';
-
 
 import Curtir from '../../utils/assets/Curtir.png';
 import Comentar from '../../utils/assets/Comentario.png';
 import MenuVertical from '../../utils/assets/MenuVertical.png';
 import Usuario from '../../utils/assets/Usuario.png';
+import Editar from '../../utils/assets/Editar.png';
+import Deletar from '../../utils/assets/Deletar.png';
 
+const Publicacao = ({ id, nome, materia, mensagem, curtidas, comentarios, onEdit, onDelete }) => {
+    const [showPopup, setShowPopup] = useState(false);
 
-// tenho a ideia de separar um select(endpoint) para as publicações e outro para os comentários 
-// pq ai quando clicar no botão de comentar ele vai fazer um select para pegar os comentários da publicação especifica 
-// (não sei como pegar o id de uma publicação especifica dps do map do feedGeral, mas to passando o id como prop para usar nessa página)
+    const togglePopup = () => {
+        setShowPopup(!showPopup);
+    };
 
-//isso precisa de uma grande refatorção no backend e banco
-
-const Publicacao = ({ id, nome, materia, mensagem, curtidas, comentarios }) => {
     return (
         <div className={Styles['container']}>
 
             <div className={Styles['header']}>
                 <div className={Styles['materiaBadge']}>{materia}</div>
-                <div className={Styles['menuVertical']}><img src={MenuVertical} alt="" /></div>
+                <div className={Styles['menuVertical']} onClick={togglePopup}>
+                    <img src={MenuVertical} alt="Menu" />
+                </div>
             </div>
 
             <div className={Styles['userInfo']}>
-                <img
-                    src={Usuario} alt="User"
-                    className={Styles['avatar']}
-                />
+                <img src={Usuario} alt="User" className={Styles['avatar']} />
                 <span className={Styles['nome']}>{nome}</span>
             </div>
 
@@ -35,22 +34,34 @@ const Publicacao = ({ id, nome, materia, mensagem, curtidas, comentarios }) => {
 
             <div className={Styles['linha']}></div>
 
-
             <div className={Styles['footer']}>
-
                 <div className={Styles['footerItem']}>
                     <span className={Styles['numero']}>{curtidas}</span>
-                    <img src={Curtir} alt="" />
+                    <img src={Curtir} alt="Curtir" />
                     <span className={Styles['footerText']}>Curtir</span>
                 </div>
-
                 <div className={Styles['footerItem']}>
                     <span className={Styles['numero']}>{comentarios}</span>
-                    <img src={Comentar} alt="" />
+                    <img src={Comentar} alt="Comentar" />
                     <span className={Styles['footerText']}>Comentar</span>
-
                 </div>
             </div>
+
+            {showPopup && (
+                <div className={Styles['popup']}>
+                    <button className={Styles['popupButton']} onClick={() => { setShowPopup(false); onEdit(id); }}>
+                        <img src={Editar} alt="" />
+                        Editar
+                    </button>
+                    
+                    <div className={Styles['linhaPopup']}></div>
+
+                    <button className={Styles['popupButton']} onClick={() => { setShowPopup(false); onDelete(id); }}>
+                        <img src={Deletar} alt="" />
+                        Excluir
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
