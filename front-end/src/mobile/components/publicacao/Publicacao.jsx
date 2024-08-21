48ada25c0401a132f36984843baf2ce1f7933798
OtavioWalk <otavio.carvalho@sptech.school>
2024-08-21 00:04:57 -0300

    feat: Add react-modal package for modal functionality,  MenuLateral component update

--- front-end/src/mobile/components/publicacao/Publicacao.jsx
+++ front-end/src/mobile/components/publicacao/Publicacao.jsx
@@ -11,23 +11,17 @@ import Editar from '../../utils/assets/Editar.png';
 import Deletar from '../../utils/assets/Deletar.png';
 import Denunciar from '../../utils/assets/Deletar.png';
 
-// import Comentario from '../comentario/Comentario';
-// import listarComentarios from '../../pages/feedGeral/FeedGeral';
-
 function formatDateTime(dateString) {
     const date = new Date(dateString);
-  
-    // Extrair hora e minutos
+
     const hours = date.getHours().toString().padStart(2, '0');
     const minutes = date.getMinutes().toString().padStart(2, '0');
-  
-    // Extrair dia, mês e ano
     const day = date.getDate().toString().padStart(2, '0');
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const year = date.getFullYear().toString().slice(-2); // Pega os últimos dois dígitos do ano
-  
+    const year = date.getFullYear().toString().slice(-2);
+
     return `${hours}:${minutes} - ${day}/${month}/${year}`;
-  }
+}
 
 function deletarPublicacao(id) {
     api.delete(`/publicacoes/${id}`)
@@ -40,84 +34,89 @@ function deletarPublicacao(id) {
         });
 }
 
-const Publicacao = ({ id, nome, materia, mensagem, horario, curtidas, comentarios, onComentariosClick, listarComentarios }) => {
+const Publicacao = ({ id, nome, materia, mensagem, horario, curtidas, comentarios, listarComentarios }) => {
     const [showPopup, setShowPopup] = useState(false);
-    
 
     const togglePopup = () => {
         setShowPopup(!showPopup);
     };
 
+    // Obtem o nome do usuário armazenado no sessionStorage
+    const nomeUsuarioLogado = sessionStorage.getItem('nome');
+
     return (
         <>
-        <div className={Styles['container']}>
+            <div className={Styles['container']}>
 
-            <div className={Styles['header']}>
-                <div className={Styles['materiaBadge']}>{materia}</div>
-                <div className={Styles['menuVertical']} onClick={togglePopup}>
-                    <img src={MenuVertical} alt="Menu" />
+                <div className={Styles['header']}>
+                    <div className={Styles['materiaBadge']}>{materia}</div>
+                    <div className={Styles['menuVertical']} onClick={togglePopup}>
+                        <img src={MenuVertical} alt="Menu" />
+                    </div>
                 </div>
+
+                <div className={Styles['userInfo']}>
+                    <img src={Usuario} alt="User" className={Styles['avatar']} />
+                    <span className={Styles['nome']}>{nome}</span>
+                </div>
+
+                <div className={Styles['mensagem']}>{mensagem}</div>
+                <div className={Styles['dataHora']}>{formatDateTime(horario)}</div>
+
+                <div className={Styles['linha']}></div>
+
+                <div className={Styles['footer']}>
+                    <div className={Styles['footerItem']}>
+                        <span className={Styles['numero']}>{curtidas}</span>
+                        <img src={Curtir} alt="Curtir" />
+                        <span className={Styles['footerText']}>Curtir</span>
+                    </div>
+                    <div className={Styles['footerItem']}>
+                        <span className={Styles['numero']}>{comentarios}</span>
+                        <img src={Comentar} alt="Comentar" />
+                        <span className={Styles['footerText']} onClick={() => { listarComentarios(id); }}>Comentários</span>
+                    </div>
+                </div>
+
+                {showPopup && (
+                    <div className={Styles['popup']}>
+                        {nomeUsuarioLogado === nome ? (
+                            <>
+                                <button className={Styles['popupButton']} onClick={() => { setShowPopup(false); }}>
+                                    <img src={Editar} alt="Editar" />
+                                    Editar
+                                </button>
+
+                                <div className={Styles['linhaPopup']}></div>
+
+                                <button className={Styles['popupButton']} onClick={() => { setShowPopup(false); deletarPublicacao(id); }}>
+                                    <img src={Deletar} alt="Deletar" />
+                                    Excluir
+                                </button>
+                            </>
+                        ) : (
+                            <button className={Styles['popupButton']} onClick={() => { setShowPopup(false); /* Lógica para denunciar */ }}>
+                                <img src={Denunciar} alt="Denunciar" />
+                                Denunciar
+                            </button>
+                        )}
+                    </div>
+                )}
+                <ToastContainer
+                    position="top-right"
+                    autoClose={1000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="colored"
+                />
             </div>
-
-            <div className={Styles['userInfo']}>
-                <img src={Usuario} alt="User" className={Styles['avatar']} />
-                <span className={Styles['nome']}>{nome}</span>
-            </div>
-
-            <div className={Styles['mensagem']}>{mensagem}</div>
-            <div className={Styles['dataHora']}>{formatDateTime(horario)}</div>
-            
-            <div className={Styles['linha']}></div>
-
-            <div className={Styles['footer']}>
-                <div className={Styles['footerItem']}>
-                    <span className={Styles['numero']}>{curtidas}</span>
-                    <img src={Curtir} alt="Curtir" />
-                    <span className={Styles['footerText']}>Curtir</span>
-                </div>
-                <div className={Styles['footerItem']}>
-                    <span className={Styles['numero']}>{comentarios}</span>
-                    <img src={Comentar} alt="Comentar" />
-                    <span className={Styles['footerText']} onClick={() => {listarComentarios(id); onComentariosClick();}}>Comentarios</span>
-                </div>
-            </div>
-
-            {showPopup && (
-                <div className={Styles['popup']}>
-                    <button className={Styles['popupButton']} onClick={() => { setShowPopup(false); }}>
-                        <img src={Editar} alt="" />
-                        Editar
-                    </button>
-
-                    <div className={Styles['linhaPopup']}></div>
-
-                    <button className={Styles['popupButton']} onClick={() => { setShowPopup(false); deletarPublicacao(id); }}>
-                        <img src={Deletar} alt="" />
-                        Excluir
-                    </button>
-
-                    <button className={Styles['popupButton']} onClick={() => { setShowPopup(false); /* Lógica para denunciar */ }}>
-                        <img src={Denunciar} alt="" />
-                        Denunciar
-                    </button>
-                </div>
-            )}
-            <ToastContainer
-                position="top-right"
-                autoClose={1000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-            />
-
-        </div>        
         </>
     );
-};
+}
 
 export default Publicacao;
--- front-end/src/mobile/pages/feedGeral/FeedGeral.jsx
+++ front-end/src/mobile/pages/feedGeral/FeedGeral.jsx
@@ -3,9 +3,11 @@ import Styles from '../feedGeral/FeedGeral.module.css';
 import api from "../../../api";
 import Publicacao from '../../components/publicacao/Publicacao';
 import Header from '../../components/headerAplicacao/Header';
-
+import Modal from 'react-modal';
 import Comentario from '../../components/comentario/Comentario';
 import fechar from '../../utils/assets/icone_x.svg';
+
+Modal.setAppElement('#root'); // Necessário para acessibilidade
 
 const subjectNameMap = {
     'MATEMATICA': 'Matemática',
@@ -52,14 +54,15 @@ const FeedGeral = () => {
             .then(response => {
                 console.log("Comentários recebidos:", response.data);
                 setComentarios(response.data);
+                setShowComentarios(true); // Abre o modal ao listar os comentários
             })
             .catch(error => {
                 console.error("Ocorreu um erro ao buscar os comentários:", error);
             });
     };
 
-    const toggleComentarios = () => {
-        setShowComentarios(!showComentarios);
+    const closeComentariosModal = () => {
+        setShowComentarios(false); // Fecha o modal
     };
 
     const publicacoesParaExibir = searchResults || publicacoes;
@@ -68,47 +71,47 @@ const FeedGeral = () => {
         <>
             <Header onSearchResult={handleSearchResult} />
             <div className={Styles['feedGeral']}>
-                {!showComentarios && (
-                    <div className={Styles['publicacoes']}>
-                        {publicacoesParaExibir.map((publicacao) => (
-                            <Publicacao
-                                key={publicacao.id}
-                                id={publicacao.id}
-                                nome={publicacao.usuario.nome}
-                                materia={formatSubjectName(publicacao.canal.nome)}
-                                mensagem={publicacao.conteudo}
-                                horario={publicacao.dataHora}
-                                curtidas={5}
-                                comentarios={publicacao.comentarios.length}
-                                onComentariosClick={toggleComentarios} // Passa a função para o componente Publicacao
-                                listarComentarios={listarComentarios}
-                            />
-                        ))}
-                    </div>
-                )}
+                <div className={Styles['publicacoes']}>
+                    {publicacoesParaExibir.map((publicacao) => (
+                        <Publicacao
+                            key={publicacao.id}
+                            id={publicacao.id}
+                            nome={publicacao.usuario.nome}
+                            materia={formatSubjectName(publicacao.canal.nome)}
+                            mensagem={publicacao.conteudo}
+                            horario={publicacao.dataHora}
+                            curtidas={5}
+                            comentarios={publicacao.comentarios.length}
+                            listarComentarios={listarComentarios}
+                        />
+                    ))}
+                </div>
             </div>
 
-            {showComentarios && (
-                <div className={Styles["comentarios"]}>
-                    <div className={Styles["fechar"]}><img src={fechar} alt="icone fechar" onClick={toggleComentarios} /></div>
-                    <div className={Styles["listaComentarios"]}>
-                        {comentariosPublicacao.length > 0 ? (
-                            comentariosPublicacao.map((comentario) => (
-                                <Comentario
-                                    key={comentario.id}
-                                    id={comentario.id}
-                                    nome={comentario.usuario.nome}
-                                    mensagem={comentario.comentario}
-                                    horario={comentario.dataHora}
-                                    curtidas={comentario.reacoes.length}
-                                />
-                            ))
-                        ): (
-                            <p className={Styles["textoSemComentarios"]}>Ainda não há comentarios nessa publicação <br/> Seja o primeiro!</p>
-                        )}
-                    </div>
+            <Modal
+                isOpen={showComentarios}
+                onRequestClose={closeComentariosModal}
+                className={Styles['comentariosModal']}
+                overlayClassName={Styles['comentariosOverlay']}
+            >
+                <div className={Styles["fechar"]}><img src={fechar} alt="icone fechar" onClick={closeComentariosModal} /></div>
+                <div className={Styles["listaComentarios"]}>
+                    {comentariosPublicacao.length > 0 ? (
+                        comentariosPublicacao.map((comentario) => (
+                            <Comentario
+                                key={comentario.id}
+                                id={comentario.id}
+                                nome={comentario.usuario.nome}
+                                mensagem={comentario.comentario}
+                                horario={comentario.dataHora}
+                                curtidas={comentario.reacoes.length}
+                            />
+                        ))
+                    ) : (
+                        <p className={Styles["textoSemComentarios"]}>Ainda não há comentários nessa publicação <br /> Seja o primeiro!</p>
+                    )}
                 </div>
-            )}
+            </Modal>
         </>
     );
 }
