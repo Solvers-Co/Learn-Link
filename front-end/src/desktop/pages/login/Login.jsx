303c928e5472ab0054655a3d90fd11e411a0ddbc
teuscapellari <mateus.capellari@sptech.school>
2024-09-09 21:21:11 -0300

    Bloqueio de login para usuários que não sejam ADM, e bloqueio de cadastro de funcionários por ADMS que não sejam a conta principal

--- front-end/src/desktop/pages/login/Login.jsx
+++ front-end/src/desktop/pages/login/Login.jsx
@@ -28,52 +28,35 @@ const Login = () => {
             const sessionData = response.data.token;
             const sessionUserId = response.data.userId;
             const sessionUserName = response.data.nome;
+            const sessionUserTipo = response.data.tipo_usuario_id;
+            const sessionUserEmail = response.data.email;
 
             sessionStorage.setItem('token', sessionData)
             sessionStorage.setItem('userId', sessionUserId)
             sessionStorage.setItem('nome', sessionUserName)
-            toast.success("Login Efetuado com sucesso!"); // Exibe uma mensagem de sucesso
-            navigate("/loginDesktop"); // Redireciona para a página de músicas
+            sessionStorage.setItem('tipo', sessionUserTipo)
+            sessionStorage.setItem('email', sessionUserEmail)
+
+            if (sessionUserTipo === 2) {
+                toast.success("Login Efetuado com sucesso!"); 
+                navigate("/dashboard"); 
+            } else {
+                toast.warning("Você não é um administrador."); 
+            }
         }).catch(() => {
             const errorMessages = [];
 
             if (!email) {
                 errorMessages.push("Email não preenchido");
             } else if (!email.includes("@")) {
-                errorMessages.push("Email deve conter @");
-            } else if (!email.includes(".") || email.indexOf("@") > email.lastIndexOf(".")) {
-                errorMessages.push("Email deve conter um domínio válido");
-            } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-                errorMessages.push("Email inválido");
-            } else if (email.trim() !== email) {
-                errorMessages.push("Email não deve conter espaços em branco no final");
-            }
+                errorMessages.push("E-mail ou senha incorretos");
+            } 
 
             // Verificações da senha
             if (!senha) {
                 errorMessages.push("Senha não preenchida");
             } else {
-                if (senha.length < 6) {
-                    errorMessages.push("Senha deve ter pelo menos 6 caracteres");
-                }
-                if (senha.length > 20) {
-                    errorMessages.push("Senha deve ter no máximo 20 caracteres");
-                }
-                if (!/[!@#$%^&*(),.?":{}|<>]/.test(senha)) {
-                    errorMessages.push("Senha deve conter pelo menos um caractere especial");
-                }
-                if (!/\d/.test(senha)) {
-                    errorMessages.push("Senha deve conter pelo menos um número");
-                }
-                if (!/[A-Z]/.test(senha)) {
-                    errorMessages.push("Senha deve conter pelo menos uma letra maiúscula");
-                }
-                if (!/[a-z]/.test(senha)) {
-                    errorMessages.push("Senha deve conter pelo menos uma letra minúscula");
-                }
-                if (senha.trim() !== senha) {
-                    errorMessages.push("Senha não deve conter espaços em branco no final");
-                }
+                errorMessages.push("E-mail ou senha incorretos");
             }
             if (errorMessages.length > 0) {
                 errorMessages.forEach(msg => toast.error(msg)); // Exibe cada mensagem de erro individualmente
@@ -91,7 +74,7 @@ const Login = () => {
                 <h3 className={styles['tituloInput']}>E-mail</h3>
                 <Input value={email} onChange={(e) => handleInputChange(e, setEmail)} />
                 <h3 className={styles['tituloInput']}>Senha</h3>
-                <Input value={senha} onChange={(e) => handleInputChange(e, setSenha)} />
+                <Input value={senha} onChange={(e) => handleInputChange(e, setSenha)} type="password" />
                 <div className={styles['divBotao']}>
                     <Botao funcao={handleClick} textoBotao="Login" />
                 </div>
