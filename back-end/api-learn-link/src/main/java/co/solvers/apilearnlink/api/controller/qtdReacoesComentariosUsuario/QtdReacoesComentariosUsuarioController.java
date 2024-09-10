4cd1a01055415bcf5d085014d6ff76302165a748
OtavioWalk <otavio.carvalho@sptech.school>
2024-09-10 20:22:12 -0300

    Feat: select de listar os 10 primeiros do ranking, implementação do back no ranking

--- back-end/api-learn-link/src/main/java/co/solvers/apilearnlink/api/controller/qtdReacoesComentariosUsuario/QtdReacoesComentariosUsuarioController.java
+++ back-end/api-learn-link/src/main/java/co/solvers/apilearnlink/api/controller/qtdReacoesComentariosUsuario/QtdReacoesComentariosUsuarioController.java
@@ -6,6 +6,9 @@ import co.solvers.apilearnlink.service.reacoesEmComentariosDoUsuario.dto.QtdCont
 import co.solvers.apilearnlink.service.reacoesEmComentariosDoUsuario.dto.QtdReacoesComentariosUsuarioDto;
 import co.solvers.apilearnlink.service.reacoesEmComentariosDoUsuario.dto.mapper.QtdContribuicoesRankingMapper;
 import co.solvers.apilearnlink.service.reacoesEmComentariosDoUsuario.dto.mapper.QtdReacoesComentariosUsuarioMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,10 +25,14 @@ import java.util.Optional;
 public class QtdReacoesComentariosUsuarioController {
 
     private final QtdReacoesComentariosUsuarioService qtdReacoesComentariosUsuarioService;
-
+    
+    @ApiResponse(responseCode = "200", description = "Quantidade de reações encontrada")
+    @ApiResponse(responseCode = "404", description = "Nenhuma quantidade de reações encontrada")
+    @Operation(summary = "Buscar quantidade de reações por comentário de um usuário", description = "Método que busca a quantidade de reações por comentário de um usuário", tags = {"Ranking"})
     @GetMapping("/buscar-nivel-de-classificacao-do-usuario/{id}")
     public ResponseEntity<Optional<QtdReacoesComentariosUsuarioDto>> listagemQtdReacoesComentarios(
-            @PathVariable Long id){
+            @PathVariable
+            @Parameter(name = "id", description = "Usuario id", example = "1") Long id) {
 
         Optional<QtdReacoesComentariosUsuarioView> qtdReacoes = qtdReacoesComentariosUsuarioService.listagemQtdReacoesComentarios(id);
 
@@ -33,15 +40,17 @@ public class QtdReacoesComentariosUsuarioController {
         return ResponseEntity.ok(qtdReacoesDto);
     }
 
+    @ApiResponse(responseCode = "200", description = "Quantidade de reações encontrada")
+    @ApiResponse(responseCode = "404", description = "Nenhuma quantidade de reações encontrada")
+    @Operation(summary = "Buscar quantidade de reações por comentário de todos os usuários", description = "Método que busca a quantidade de reações por comentário de todos os usuários", tags = {"Ranking"})
     @GetMapping("/buscar-nivel-de-classificacao-de-todos-usuarios")
-    public ResponseEntity<List<QtdContribuicoesRankingDto>> qtdContribuicoesRanking(){
+    public ResponseEntity<List<QtdContribuicoesRankingDto>> qtdContribuicoesRanking() {
+
         List<QtdReacoesComentariosUsuarioView> qtdContribuicoesRanking = qtdReacoesComentariosUsuarioService.qtdContribuicoesRanking();
 
         List<QtdContribuicoesRankingDto> dtos = QtdContribuicoesRankingMapper.toDto(qtdContribuicoesRanking);
         return ResponseEntity.ok(dtos);
     }
-
-
 
 
 }
