06a115e78a6bd1fa1e62346a64b9d63fd3722e85
JoaoVitorFB <joao.batista@sptech.school>
2024-10-16 19:34:10 -0300

    add: infraestrura terraform separada por servicos

---
+++ terraform/rt.tf
@@ -0,0 +1,33 @@
+resource "aws_route_table" "rt_vpc_learnlink" {
+  vpc_id = aws_vpc.vpc_learnlink.id
+
+  tags = {
+    Name = "rt_vpc_learnlink"
+  }
+}
+
+
+// Criando uma rota para a internet gateway "igw_vpc_learnlink" na route table "rt_vpc_learnlink"
+// A rota é para qualquer destino (0.0.0.0/0)
+
+resource "aws_route" "route" {
+  route_table_id         = aws_route_table.rt_vpc_learnlink.id
+  gateway_id             = aws_internet_gateway.igw_vpc_learnlink.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+
+// Associando a route table "rt_vpc_learnlink" com a sub-rede "sn_learnlink_public"
+
+resource "aws_route_table_association" "rt_sn_learnlink_public" {
+  subnet_id      = aws_subnet.sn_learnlink_public.id
+  route_table_id = aws_route_table.rt_vpc_learnlink.id
+}
+
+
+// Associando a route table "rt_vpc_learnlink" com a sub-rede "sn_learnlink_private"
+
+resource "aws_route_table_association" "rt_sn_learnlink_private" {
+  subnet_id      = aws_subnet.sn_learnlink_private.id
+  route_table_id = aws_route_table.rt_vpc_learnlink.id
+}
---
+++ terraform/security_group.tf
@@ -0,0 +1,51 @@
+resource "aws_security_group" "sg_learnlink_backend" {
+  vpc_id = aws_vpc.vpc_learnlink.id
+  name   = "sg_learnlink_backend"
+
+  ingress {
+    from_port   = var.porta_ssh
+    to_port     = var.porta_ssh
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "sg_learnlink_frontend" {
+  vpc_id = aws_vpc.vpc_learnlink.id
+  name   = "sg_learnlink_frontend"
+
+  ingress {
+    from_port   = var.porta_ssh
+    to_port     = var.porta_ssh
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = var.porta_http
+    to_port     = var.porta_http
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = var.porta_http
+    to_port     = var.porta_http
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = var.porta_https
+    to_port     = var.porta_https
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = var.porta_https
+    to_port     = var.porta_https
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+} 
