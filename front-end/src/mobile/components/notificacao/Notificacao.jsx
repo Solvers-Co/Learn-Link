a92819c737fa9cfcb3c51556469bcd4931076991
KauanCursino <kauan.souza@sptech.school>
2024-10-29 20:07:49 -0300

    feat: fotos aparecendo no ranking e nas notificações

--- front-end/src/mobile/components/notificacao/Notificacao.jsx
+++ front-end/src/mobile/components/notificacao/Notificacao.jsx
@@ -1,6 +1,6 @@
 import styles from './Notificacao.module.css';
 import { generateInitials } from '../../utils/functions/GerarIniciais';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import api from "../../../api"
 
@@ -9,6 +9,7 @@ const Notificacao = ({ corDeFundo, id, corpo, nomeUsuarioGerador, vista }) => {
     const [conteudo, setConteudo] = useState(corpo);
     const [nomeUsuario, setNomeUsuarioGerador] = useState(nomeUsuarioGerador);
     const [visualizada, setVisusalizada] = useState(vista)
+    const [srcImagemPerfil, setSrcImagemPerfil] = useState('')
     const avatar = generateInitials(nomeUsuario);
     const backgroundColor = visualizada === 1 ? corDeFundo : 'white';
 
@@ -20,13 +21,30 @@ const Notificacao = ({ corDeFundo, id, corpo, nomeUsuarioGerador, vista }) => {
         })
     }
 
+    useEffect(() => {
+        async function buscarImagemPerfil() {
+            try {
+                const response = await api.get(`usuarios/buscar-imagem-perfil/${sessionStorage.getItem("userId")}`);
+                console.log(response.data)
+                setSrcImagemPerfil(response.data)
+                // setSrcImagemPerfil("https://s3-learnlink.s3.us-east-1.amazonaws.com/WIN_20240909_09_30_09_Pro.jpg")
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        buscarImagemPerfil();
+    }, [])
 
     return (
         <div className={styles.notificacao} onClick={fetchVisualizarNotificacao}>
                 <div className={styles.notificacaoItem} style={{ backgroundColor }}>
                     <div className={styles.infos}>
                         <div className={styles.avatar}>
-                            {avatar}
+                        {srcImagemPerfil ? (
+                                <img className={styles.imagemPerfil} src={srcImagemPerfil} alt="Imagem de Perfil" />
+                            ) : (
+                                <span className={styles.avatar}>{avatar}</span>
+                            )}  
                         </div>
                         <div className={styles.texto}>
                             <span className={styles.nome}>{nomeUsuario}</span>
