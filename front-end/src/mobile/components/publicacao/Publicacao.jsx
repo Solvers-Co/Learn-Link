43b048729114a3a2723955c6fec092af4c2b1ef5
OtavioWalk <otavio.carvalho@sptech.school>
2024-11-19 20:09:39 -0300

    feat: refatoração no carregamento da edição de imagem

--- front-end/src/mobile/components/publicacao/Publicacao.jsx
+++ front-end/src/mobile/components/publicacao/Publicacao.jsx
@@ -79,40 +79,54 @@ function deletarPublicacao(id) {
         });
 }
 
- function editarPublicacao(id, novoConteudo, novoCanal) {
-    if(sessionStorage.getItem("bytesImagemPublicacao")){
-
+async function editarPublicacao(id, novoConteudo, novoCanal) {
+    if (sessionStorage.getItem("bytesImagemPublicacao")) {
         var imagemUrl = [];
-        var byteArray = Uint8Array.from(atob(sessionStorage.getItem("bytesImagemPublicacao")), c => c.charCodeAt(0));
+        var byteArray = Uint8Array.from(
+            atob(sessionStorage.getItem("bytesImagemPublicacao")),
+            (c) => c.charCodeAt(0)
+        );
 
         for (let i = 0; i < byteArray.length; i++) {
-            imagemUrl.push(byteArray[i])
+            imagemUrl.push(byteArray[i]);
         }
 
         sessionStorage.removeItem("bytesImagemPublicacao");
 
         const imagemPerfilDto = {
-            imagemBytes: imagemUrl
-        }
+            imagemBytes: imagemUrl,
+        };
 
-        api.patch(`/publicacoes/${id}/conteudo?novoConteudo=${encodeURIComponent(novoConteudo)}&novoCanal=${encodeURIComponent(novoCanal)}`, imagemPerfilDto)
-        .then(response => {
+        try {
+            await api.patch(
+                `/publicacoes/${id}/conteudo?novoConteudo=${encodeURIComponent(
+                    novoConteudo
+                )}&novoCanal=${encodeURIComponent(novoCanal)}`,
+                imagemPerfilDto
+            );
             toast.success("Publicação editada com sucesso!");
             window.location.reload();
-        })
-        .catch(error => {
-            console.error("Erro ao editar a publicação:", error.response?.data || error.message);
-        });    
-    }else{
-        api.patch(`/publicacoes/${id}/conteudo?novoConteudo=${encodeURIComponent(novoConteudo)}&novoCanal=${encodeURIComponent(novoCanal)}`)
-            .then(response => {
-                toast.success("Publicação editada com sucesso!");
-                window.location.reload();
-            })
-            .catch(error => {
-                console.error("Erro ao editar a publicação:", error.response?.data || error.message);
-            });
-
+        } catch (error) {
+            console.error(
+                "Erro ao editar a publicação:",
+                error.response?.data || error.message
+            );
+        }
+    } else {
+        try {
+            await api.patch(
+                `/publicacoes/${id}/conteudo?novoConteudo=${encodeURIComponent(
+                    novoConteudo
+                )}&novoCanal=${encodeURIComponent(novoCanal)}`
+            );
+            toast.success("Publicação editada com sucesso!");
+            window.location.reload();
+        } catch (error) {
+            console.error(
+                "Erro ao editar a publicação:",
+                error.response?.data || error.message
+            );
+        }
     }
 }
 
@@ -181,7 +195,6 @@ const Publicacao = ({ quemCurtiu, id, nome, materia, mensagem, horario, curtidas
 
     const confirmarEdicao = () => {
         editarPublicacao(id, novoConteudo, novaMateria);
-        closeEditarModal();
     };
 
     const abrirDenunciaModal = () => {
