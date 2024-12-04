fd361caedecd3ae9e918318a19d64b12fbbceae4
sofhiautaka <sofhia.utaka@sptech.school>
2024-12-03 21:24:51 -0300

    fix: erro para enviar email consertado

--- front-end/src/desktop/components/cadastroFuncionarios/input/Input.jsx
+++ front-end/src/desktop/components/cadastroFuncionarios/input/Input.jsx
@@ -1,10 +1,11 @@
 import styles from './Input.module.css'
 
-const Input = ({ label, placeholder, onChange }) => {
+const Input = ({ label, placeholder, onChange, value }) => {
     return (
         <div className={styles.container}>
             <label className={styles.label}>{label}</label>
-            <input className={styles.input} placeholder={placeholder} />
+            <input className={styles.input} placeholder={placeholder} value={value}
+            onChange={onChange}/>
         </div>
     )
 }
